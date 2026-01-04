3b0dd070572815448f034ab1cf148f78b9a5d36e
Abdalla <abdalla20736@yahoo.com>
2026-01-04 10:32:12 +0200

    completed-project

--- assets/js/planets.js
+++ assets/js/planets.js
@@ -34,6 +34,14 @@ let planetsData;
 
 async function DisplayPlanets() {
   planetsData = await api.GetPlanets();
+  if (planetsData.error) {
+    planetTbody.innerHTML = "";
+    planetsGrid.innerHTML = `<div class="col-span-full text-center py-8">
+                    <i class="text-red-400 text-4xl mb-4" data-fa-i2svg=""><svg class="svg-inline--fa fa-triangle-exclamation" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="triangle-exclamation" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg></i>
+                    <p class="text-slate-400">Failed to load planets data. Please try again later.</p>
+                </div>`;
+    return;
+  }
   DisplayPlanetsGrid(planetsData);
   DisplayPlanetsTable();
   DisplayPlanetInfo(6);
@@ -117,6 +125,7 @@ function DisplayPlanetsTable() {
     ["Venus", "#f97316"],
   ]);
   let planetRows = "";
+
   planetsData.bodies.forEach((planet) => {
     planetRows += ` <tr class="hover:bg-slate-800/30 transition-colors">
                       <td
--- assets/js/todayspace.js
+++ assets/js/todayspace.js
@@ -32,8 +32,9 @@ function DisplayTodayInfo() {
 async function DisplayInfoByDate() {
   ShowLoading();
   let blog = await api.GetBlogByDate(apodDate.value);
+  console.log(blog);
+  OnLoadingComplete(blog);
   updateSpaceBlog(blog);
-  HideLoading();
 }
 
 function updateSpaceBlog(blog) {
@@ -69,7 +70,18 @@ function ShowLoading() {
   apodImage.classList.add("hidden");
 }
 
-function HideLoading() {
+function OnLoadingComplete(blog) {
+  if (!blog) {
+    apodLoading.innerHTML = `
+
+            <i class="text-4xl text-red-400 mb-4" data-fa-i2svg=""><svg class="svg-inline--fa fa-triangle-exclamation" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="triangle-exclamation" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg></i>
+            <p class="text-slate-400">Failed to load today's image</p>
+            <p class="text-slate-500 text-sm mt-2">Please try again later</p>
+       
+   `;
+
+    return;
+  }
   apodLoading.classList.add("hidden");
   apodImage.classList.remove("hidden");
 }
